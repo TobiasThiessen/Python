0135be271881ee82f952125a1137eecf7b236e5f
TobiasThiessen <60485998+TobiasThiessen@users.noreply.github.com>
2022-07-09 03:55:49 +0200

    Add files via upload

---
+++ TensorflowOD_CustomModel/scripts/imageLabel_list.py
@@ -0,0 +1,9 @@
+imagelabels = [{"name":"Placeholder", "id":1}, 
+               {"name":"Placeholder", "id":2},
+               {"name":"Placeholder", "id":3},
+               {"name":"Placeholder", "id":4},
+               {"name":"Placeholder", "id":5},
+               {"name":"Placeholder", "id":6},
+               {"name":"Placeholder", "id":7},
+               {"name":"Placeholder", "id":8},
+               {"name":"Placeholder", "id":9}]
