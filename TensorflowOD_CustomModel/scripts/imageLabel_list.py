38021902a236d632acbb8f358cbd374222d46257
TobiasThiessen <60485998+TobiasThiessen@users.noreply.github.com>
2022-07-09 04:32:41 +0200

    Add files via upload

--- TensorflowOD_CustomModel/scripts/imageLabel_list.py
+++ TensorflowOD_CustomModel/scripts/imageLabel_list.py
@@ -1,9 +1,9 @@
-imagelabels = [{"name":"Placeholder", "id":1}, 
-               {"name":"Placeholder", "id":2},
-               {"name":"Placeholder", "id":3},
-               {"name":"Placeholder", "id":4},
-               {"name":"Placeholder", "id":5},
-               {"name":"Placeholder", "id":6},
-               {"name":"Placeholder", "id":7},
-               {"name":"Placeholder", "id":8},
-               {"name":"Placeholder", "id":9}]
+imagelabels = [{"name":"F_FACE", "id":1}, 
+               {"name":"F_TITS", "id":2},
+               {"name":"F_CLEAVAGE", "id":3},
+               {"name":"F_BRA", "id":4},
+               {"name":"F_FOOT", "id":5},
+               {"name":"F_VAG", "id":6},
+               {"name":"F_PANTIES", "id":7},
+               {"name":"F_ASS", "id":8},
+               {"name":"F_ANUS", "id":9}]
